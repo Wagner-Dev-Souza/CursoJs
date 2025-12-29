c4e1efcf27f73dc7d733169f3c5683337c6f7aa0
Wagner-Dev-Souza <souzawagner.ti@gmail.com>
2025-12-29 12:24:51 -0300

    Switch_Case

---
+++ Aula24_Switch_Case/index.js
@@ -0,0 +1,91 @@
+const data = new Date('1987-04-25 00:00:00');
+let diaSemana = data.getDay();
+//diaSemana = 7;
+let diaSemanaTexto;
+
+
+//Utilizando IF:
+/*
+if (diaSemana === 0){
+    diaSemanaTexto = 'Domingo';
+} else if (diaSemana === 1) {
+    diaSemanaTexto = 'Segunda';
+} else if (diaSemana === 2) {
+    diaSemanaTexto = 'Terça';
+} else if (diaSemana === 3) {
+    diaSemanaTexto = 'Quarta';
+} else if (diaSemana === 4) {
+    diaSemanaTexto = 'Quinta';
+} else if (diaSemana === 5) {
+    diaSemanaTexto = 'Sexta';
+} else if (diaSemana === 6) {
+    diaSemanaTexto = 'Sábado';
+} else {
+    diaSemanaTexto = 'Error';
+}
+*/
+
+//Utilizando o Switch Case
+/*
+switch (diaSemana) {
+case 0:
+    diaSemanaTexto = 'Domingo';
+    break;
+case 1:
+    diaSemanaTexto = 'Segunda';
+    break;
+case 2:
+    diaSemanaTexto = 'Terça';
+    break;
+case 3:
+    diaSemanaTexto = 'Quarta';
+    break;
+case 4:
+    diaSemanaTexto = 'Quinta';
+    break;
+case 5:
+    diaSemanaTexto = 'Sexta';
+    break;
+case 6:
+    diaSemanaTexto = 'Sábado';
+    break;
+default:
+    diaSemanaTexto = 'Error';
+}
+*/
+
+//Utilizando Switch Case na Função:
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        return diaSemanaTexto;
+    case 1:
+        diaSemanaTexto = 'Segunda';
+        return diaSemanaTexto;
+    case 2:
+        diaSemanaTexto = 'Terça';
+        return diaSemanaTexto;
+    case 3:
+        diaSemanaTexto = 'Quarta';
+        return diaSemanaTexto;
+    case 4:
+        diaSemanaTexto = 'Quinta';
+        return diaSemanaTexto;
+    case 5:
+        diaSemanaTexto = 'Sexta';
+        return diaSemanaTexto;
+    case 6:
+        diaSemanaTexto = 'Sábado';
+        return diaSemanaTexto;
+    default:
+        diaSemanaTexto = 'Error';
+        return diaSemanaTexto;
+    }
+}
+
+diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+console.log(diaSemana, diaSemanaTexto);
