5d2ac610c20dc6b25d84107e6680e63ab59a4881
Wagner-Dev-Souza <souzawagner.ti@gmail.com>
2026-01-15 20:22:49 -0300

    desestruturação de arrays

---
+++ aula26_atribuição por desestruturação/index.js
@@ -0,0 +1,31 @@
+//Desestruturação simples:
+let a = "A" // B
+let b = "B" // C
+let c = "C" // A
+
+console.log(a, b, c);
+
+const letras = [b, c, a];
+[a, b, c] = letras;
+
+console.log(a, b, c);
+
+//Desestruturação simples de array
+const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const [um, dois, tres, ...resto] = numeros; //rest operator
+const [primeiro, , terceiro, , quinto, ...outros] = numeros;
+console.log(um, dois, tres);
+console.log(resto);
+console.log(primeiro, terceiro, quinto);
+console.log(outros);
+
+//desestruturação de array de arrays
+const array_num = [[1, 2, 3], [4 , 5, 6], [7, 8, 9]];
+//para pegar o numero 6:
+console.log(array_num[1][2])
+//pegando o seis por desestruturação (metodo complexo)
+const [,[,,seis]] = array_num;
+console.log(seis);
+//forma mais simple
+const [lista1, lista2, lista3] = array_num;
+console.log(lista2[2]);
